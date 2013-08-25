c69401edb2713780d3be7c7aac2a1138545d439d
prolic <saschaprolic@googlemail.com>
2013-08-25 21:30:49 +0200

    test bootstrap without composer requirement

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -2,21 +2,23 @@
 
 ini_set('error_reporting', E_ALL);
 
-$files = array(__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php');
+// init PHPUnit
+require_once 'PHPUnit/Framework/TestCase.php';
+if ('@package_version@' !== PHPUnit_Runner_Version::id() && version_compare(PHPUnit_Runner_Version::id(), '3.6.0', '<')) {
+    echo 'This version of PHPUnit (' . PHPUnit_Runner_Version::id() . ') is not supported.' . PHP_EOL;
+    exit(1);
+}
 
-foreach ($files as $file) {
-    if (file_exists($file)) {
-        $loader = require $file;
-
-        break;
+function autoload($className)
+{
+    $test = dirname(__FILE__) . '/../src/' . str_replace('_', '/', $className) . '.php';
+    if (file_exists($test)) {
+        require $test;
+    }
+    $test = dirname(__FILE__) . '/' . str_replace('_', '/', $className) . '.php';
+    if (file_exists($test)) {
+        require $test;
     }
 }
 
-if (! isset($loader)) {
-    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
-}
-
-/* @var $loader \Composer\Autoload\ClassLoader */
-$loader->add('MabeEnumTest_', __DIR__);
-
-unset($files, $file, $loader);
+spl_autoload_register('autoload');
