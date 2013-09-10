86e6dd846a9124ee6660cfa7d4b3dd8b5047bf49
Marc Bennewitz <marc-bennewitz@arcor.de>
2013-09-10 21:00:28 +0200

    Namespaces: As of the singleton implementation requires PHP 5.3 we can switch to namespaces

--- tests/MabeEnumTest/TestAsset/EnumInheritance.php
+++ tests/MabeEnumTest/TestAsset/EnumInheritance.php
@@ -1,13 +1,15 @@
 <?php
 
+namespace MabeEnumTest\TestAsset;
+
 /**
- * Unit tests for the class MabeEnum_Enum
+ * Unit tests for the class MabeEnum\Enum
  *
  * @link http://github.com/marc-mabe/php-enum for the canonical source repository
  * @copyright Copyright (c) 2012 Marc Bennewitz
  * @license http://github.com/marc-mabe/php-enum/blob/master/LICENSE.txt New BSD License
  */
-class MabeEnumTest_TestAsset_EnumInheritance extends MabeEnumTest_TestAsset_EnumWithoutDefaultValue
+class EnumInheritance extends EnumWithoutDefaultValue
 {
     const INHERITANCE = 'Inheritance';
 }
