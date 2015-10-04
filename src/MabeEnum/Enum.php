603d138da52b6c3e9cbed194d377d5bf3b8fbe47
Marc Bennewitz <marc@mabe.berlin>
2015-10-04 19:33:33 +0200

    better description for Enum::has

--- src/MabeEnum/Enum.php
+++ src/MabeEnum/Enum.php
@@ -259,7 +259,7 @@ abstract class Enum
     }
 
     /**
-     * Check for given value
+     * Is the given enumerator part of this enumeration
      *
      * @param static|null|bool|int|float|string $value
      * @return bool
