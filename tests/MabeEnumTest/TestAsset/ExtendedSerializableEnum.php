02e9a2fd0f7e6e1ce09e0e28e65ce7168e42b13f
Marc Bennewitz <marc@mabe.berlin>
2019-05-21 21:59:12 +0200

    fix call static in SerializableTrait

---
+++ tests/MabeEnumTest/TestAsset/ExtendedSerializableEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MabeEnumTest\TestAsset;
+
+/**
+ * Extended serializable enumeration
+ *
+ * @link http://github.com/marc-mabe/php-enum for the canonical source repository
+ * @copyright Copyright (c) 2017 Marc Bennewitz
+ * @license http://github.com/marc-mabe/php-enum/blob/master/LICENSE.txt New BSD License
+ */
+class ExtendedSerializableEnum extends SerializableEnum
+{
+    const EXTENDED = 'extended';
+}
